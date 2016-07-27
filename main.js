ff9c90d42f6682edea755c811f9cc1036d3c0683
mramachandra <mramachandra@deloitte.com>
2016-07-27 19:52:39 +0530

    updated port number

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -9,10 +9,9 @@ var app = express();
 app.get('/', function (req, res) {
     console.log(req);
     res.send('Hello World!');
-  
+
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT, function () {
   console.log('Example app listening on port 3000!');
 });
-
