22ef7a3f363f3cf1d0befe96f405a1ecfacc0e35
mramachandra <mramachandra@deloitte.com>
2016-07-30 21:43:53 +0530

    s

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 var TOKEN = '';
-//var http = require('http');
 var request = require('request');
 
 
@@ -20,7 +19,11 @@ app.post('/token', function (req, res) {
     //res.send(req.body);
     TOKEN = req.body.token;
     console.log('token : '+TOKEN);
-    getIdentifier(res);
+    if(TOKEN != ''){
+      getIdentifier(res);
+    }else {
+      res.send('nothing found');
+    }
     //res.send(req.body);
 });
 
@@ -35,26 +38,6 @@ var server = app.listen(process.env.PORT, function () {
 
 
 function getIdentifier(res){
-/*
-  var options = {
-    host: 'rpxnow.com',
-    port: 80,
-    path: '/api/v2/auth_info?apiKey=90ef369262e67fe16d54c454afcf1b6fb11e8d07&token='+TOKEN,
-    method: 'GET'
-  };
-
-  http.request(options, function(res) {
-    console.log('STATUS: ' + res.statusCode);
-    console.log('HEADERS: ' + JSON.stringify(res.headers));
-    res.setEncoding('utf8');
-    res.on('data', function (chunk) {
-      console.log('BODY: ' + chunk);
-      //res.send(chunk);
-    });
-  }).end();
-*/
-
-  //var request = require('request');
   var url = 'http://rpxnow.com/api/v2/auth_info?apiKey=90ef369262e67fe16d54c454afcf1b6fb11e8d07&token='+TOKEN;
   request(url, function (error, response, body) {
       if (!error && response.statusCode == 200) {
