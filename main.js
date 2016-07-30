83f667b515471ed9f14e992d24c4b3d455bb581a
mramachandra <mramachandra@deloitte.com>
2016-07-30 20:30:16 +0530

    bb

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var app = express();
 var fs = require("fs");
 var TOKEN = '';
 
+
 var bodyParser = require('body-parser');
 
 app.use(express.static('public'));
@@ -27,3 +28,23 @@ var server = app.listen(process.env.PORT, function () {
   console.log("Example app listening at http://"+host+":"+port);
 
 });
+
+
+function getIdentifier(res){
+  var options = {
+    host: 'https://rpxnow.com',
+    port: 80,
+    path: '/api/v2/auth_info?apiKey=90ef369262e67fe16d54c454afcf1b6fb11e8d07&token='+TOKEN,
+    method: 'POST'
+  };
+
+  http.request(options, function(res) {
+    console.log('STATUS: ' + res.statusCode);
+    console.log('HEADERS: ' + JSON.stringify(res.headers));
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+      console.log('BODY: ' + chunk);
+      res.send(chunk);
+    });
+  }).end();
+}
